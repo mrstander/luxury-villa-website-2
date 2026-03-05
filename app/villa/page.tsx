04fbe8893b7abc19134f4f0d572551c26ffe5aae
GavinStander <mrgavinstander@gmail.com>
2026-03-05 21:36:32 +0200

    remove searchbar

--- app/villa/page.tsx
+++ app/villa/page.tsx
@@ -97,7 +97,7 @@ export default function VillaPage() {
                 </div>
             </section>
 
-            <BookingSearchBlock />
+
 
             {/* ================= INTRO ================= */}
             <section className="max-w-6xl mx-auto px-6 py-16">
